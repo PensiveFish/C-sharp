589610751a340613fbf68f679d0cc470ccd5b7ee
Edgar <40713378+PensiveFish@users.noreply.github.com>
2018-07-02 21:38:16 +0300

    Delete MazeCell.cs

--- Maze Generator/MazeCell.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class MazeCell
-{
-    public bool visited; //false for hunt and kill alghoritm
-    public GameObject northWall, eastWall, southWall, westWall, floor; //each cell have it's 4 walls and floor, you can add here ceiling as well
-}
